47b0bb9bb7c530e59ff2f43bf8b60459405720e4
Ryan Mitchell <rtmitchell@google.com>
2019-10-10 10:21:46 -0700

    sdm845-common: Use component-override to configure Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default.
    
    Remove usages of profile_supported_* from overlays as they no
    longer configure the enable state of Bluetooth componenets.
    
    Bug: 135048762
    Test:
          adb shell pm query-services \
          com.android.bluetooth/.avrcp.AvrcpTargetService
          adb shell pm query-services \
          com.android.bluetooth/.avrcpcontroller.AvrcpControllerService
          adb shell pm query-services \
          com.android.bluetooth/.ba.BATService
          adb shell pm query-services \
          com.android.bluetooth/.hid.HidDeviceService
          adb shell pm query-services \
          com.android.bluetooth/.hid.HidHostService
          adb shell pm query-services \
          com.android.bluetooth/.sap.SapService
    
    Change-Id: I284d254a921b14d1533745611a083f4b5a27d9dc

--- sdm845.mk
+++ sdm845.mk
@@ -17,16 +17,16 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # NusantaraParts
 $(call inherit-product, packages/apps/NusantaraParts/nadproject.mk)
 
+# Component overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-system
 
 # Permissions
 PRODUCT_COPY_FILES += \
