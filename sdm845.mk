e150ccb2b2cf6e00082e0bd832d39f7e693c6b30
argraur <argraur@gmail.com>
2020-01-25 22:27:42 +0530

    sdm845-common: Use Pixel charger animation

--- sdm845.mk
+++ sdm845.mk
@@ -236,6 +236,11 @@ PRODUCT_PACKAGES += \
     libandroid_net \
     netutils-wrapper-1.0
 
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
