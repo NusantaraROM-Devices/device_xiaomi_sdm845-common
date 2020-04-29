9cbeb11e08e1140bcccf3d867111880a0795bd44
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-04-28 22:31:54 -0300

    sdm845-common: Nuke CAF power HAL
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- sdm845.mk
+++ sdm845.mk
@@ -283,13 +283,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_sdm845
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
