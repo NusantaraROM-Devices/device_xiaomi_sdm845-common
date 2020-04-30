6417f6d515fb9efd284487ac085ed9b12ee1744d
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-04-30 17:15:04 -0300

    sdm845-common: Build libperfmgr HAL
    * Hur durr....
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- sdm845.mk
+++ sdm845.mk
@@ -236,6 +236,13 @@ PRODUCT_PACKAGES += \
     libandroid_net \
     netutils-wrapper-1.0
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.3-service.xiaomi-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
