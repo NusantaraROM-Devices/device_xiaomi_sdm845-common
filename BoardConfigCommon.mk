LineageOS/android_device_xiaomi_sdm845-common

sdm845-common: Split system, odm and vendor props
Change-Id: Iee726d7d95d2bc4abb14fcfdffc55a04aca6e1cc

BoardConfigCommon.mk
@@ -114,6 +114,12 @@ TARGET_USES_MKE2FS := true
 
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Properties
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+TARGET_ODM_PROP += $(VENDOR_PATH)/odm.prop
+TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
+TARGET_VENDOR_PROP += $(VENDOR_PATH)/vendor.prop
+
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
 BUILD_WITHOUT_VENDOR := true

NusantaraROM-Devices/device_xiaomi_sdm845-common

Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

BoardConfigCommon.mk
@@ -127,7 +127,9 @@ TARGET_USES_MKE2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Properties
-TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true