f01d1f14a250d75bcc51a749a0f6edab6e3e41ec
goodmeow <harunbam3@gmail.com>
2020-08-15 15:24:14 +0700

    sdm845-common: thermal: rebrand from pixel to sdm845 - also restore back header file to CROSSHATCH - move .json to configs folder
    Signed-off-by: goodmeow <harunbam3@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -353,11 +353,11 @@ PRODUCT_PACKAGES += \
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.dipper \
+    android.hardware.thermal@2.0-service.xiaomi_sdm845 \
 
 # Thermal HAL config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
 
 # Tracing
 PRODUCT_PACKAGES += \
