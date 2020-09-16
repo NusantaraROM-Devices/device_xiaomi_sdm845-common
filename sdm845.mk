3ad4ffc466ef87f074f633c6a5ddc826c6826c19
Ritesh Saxena <riteshsax007@gmail.com>
2020-09-16 20:13:21 +0700

    sdm845-common: Switch to thermal hal 2.0
        Change-Id: Idf1f9ad4c891dc572a8b42c1a748d930d6bd30b3

--- sdm845.mk
+++ sdm845.mk
@@ -294,9 +294,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm845
+    android.hardware.thermal@2.0-service.mock
 
 # Trust HAL
 PRODUCT_PACKAGES += \
