4910b2bf4874efb952ace248b627cc2dcbb5a942
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    sdm845-common: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -238,8 +238,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-impl \
-    android.hardware.power@1.2-service
+    android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
