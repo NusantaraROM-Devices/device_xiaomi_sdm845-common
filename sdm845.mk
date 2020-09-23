e9391220020a2e1a879487e8b68d0c707b61923f
vitor00almei <pondebatata@outlook.com>
2020-09-23 11:17:21 +0000

    sdm845-common: convert libhidl to blueprint

--- sdm845.mk
+++ sdm845.mk
@@ -169,10 +169,7 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.base@1.0
 
 PRODUCT_PACKAGES += \
     libhwbinder \
