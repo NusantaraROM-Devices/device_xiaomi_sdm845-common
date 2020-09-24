fec977ad86e955430deec19eb280f8eda8cd6c87
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-24 23:40:33 +0300

    sdm845-common: switch to legacy sensor hal

--- sdm845.mk
+++ sdm845.mk
@@ -278,8 +278,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-impl-legacy \
+    android.hardware.sensors@1.0-service.legacy \
     libsensorndkbridge
 
 # Soong namespaces
