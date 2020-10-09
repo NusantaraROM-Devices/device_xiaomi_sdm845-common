525ec860e8fbf5d7bd12ae59638d6e4bd81628d2
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-09 23:08:47 +0530

    sdm845-common: Configure cpusets for dex2oat
     * use all 8 cpus for all operations
     * https://source.android.com/devices/tech/dalvik/configure

--- sdm845.mk
+++ sdm845.mk
@@ -143,6 +143,15 @@ PRODUCT_PACKAGES += \
     android.hardware.contexthub@1.0-impl.generic \
     android.hardware.contexthub@1.0-service
 
+# Configure dex2oat
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.dex2oat-threads=8 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-threads=8
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts \
