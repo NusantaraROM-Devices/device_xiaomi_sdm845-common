44d015c2b2b10b4561e84ecf0e338008b52070f9
goodmeow <harunbam3@gmail.com>
2020-10-31 01:36:56 +0700

    [squash]sdm845-common: init bloatware
    sdm845-common: add GalleryGo & KimciRecorder as prebuilt
    
    sdm845-common: add gcam as prebuilt
    
    reorder all prebuilt
    
    sdm845-common: prebuilts: build them all

--- sdm845.mk
+++ sdm845.mk
@@ -121,7 +121,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Common init scripts
@@ -145,7 +144,10 @@ PRODUCT_PACKAGES += \
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
-    XiaomiParts
+    XiaomiParts \
+    Gcam \
+    GalleryGo \
+    KimciRecorder
 
 # Display
 PRODUCT_PACKAGES += \
