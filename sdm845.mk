a0aebb3e3f4677b2347aa3a0ba99825783ebc30f
goodmeow <harunbam3@gmail.com>
2020-11-14 23:46:52 +0700

    Revert "sdm845-common: switch to legacy sensor hal"
    This reverts commit fec977ad86e955430deec19eb280f8eda8cd6c87.

--- sdm845.mk
+++ sdm845.mk
@@ -300,8 +300,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl-legacy \
-    android.hardware.sensors@1.0-service.legacy \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
 # Soong namespaces
