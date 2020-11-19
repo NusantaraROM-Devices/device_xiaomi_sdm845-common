291d1909f0d891825252c170df8700e66baebb3d
Bruno Martins <bgcngm@gmail.com>
2020-11-19 00:32:09 +0000

    sdm845-common: Remove unused jars and 32-bit radio libs
    Change-Id: I651edb4823e54b589b7b72f6362a07c6e78b955a
    
    *remove kanged blobs too
    
    Signed-off-by: goodmeow <harunbam3@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -361,7 +361,6 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite-vendorcompat
 
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
 
 # VR
