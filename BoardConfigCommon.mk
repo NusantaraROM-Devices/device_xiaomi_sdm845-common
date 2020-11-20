937facabd582b9edb636fa66ed6e1360dd828eae
goodmeow <harunbam3@gmail.com>
2020-11-20 23:01:08 +0700

    Revert "sdm845-common: power: Adapt extension to new aidl power HAL"
    This reverts commit ed255c9e54a714cf9d02cfe24377dbd395f31d3a.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,7 +128,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
 TARGET_USES_NON_LEGACY_POWERHAL := true
-TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+TARGET_POWER_SET_FEATURE_LIB := libpower_feature.xiaomi_sdm845
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
