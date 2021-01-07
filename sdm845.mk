d9505da49d9cfe12692bce0b4156beee7172c2ed
goodmeow <harunbam3@gmail.com>
2021-01-07 21:59:43 +0700

    Revert "sdm845-common: remove ifaa for a while"
    This reverts commit 63a1d51100933b355fcd3992f0078e3d707aac4e.

--- sdm845.mk
+++ sdm845.mk
@@ -215,6 +215,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/sdm845-tavil-snd-card_Button_Jack.kl:system/usr/keylayout/sdm845-tavil-snd-card_Button_Jack.kl
