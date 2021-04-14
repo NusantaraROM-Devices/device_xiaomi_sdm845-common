2064efbcff4f7ccbb5c07c4338798ca91709cc1c
goodmeow <goodmeow@nusantaradevs.org>
2021-04-14 13:28:10 +0700

    sdm845-common: build livedisplay
    Change-Id: I45b7b80ed4ca940e6e1980dce17de480928ba8c6

--- sdm845.mk
+++ sdm845.mk
@@ -219,6 +219,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm845
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
