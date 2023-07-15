ff0a53b163283e1b75e12fd75817546e92fa3ce2
zkyzzz <muhadlim17@gmail.com>
2023-07-15 11:26:31 +0700

    sdm845-common: Adapt to NusantaraProject

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -15,7 +15,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 HAVOC_ROOT="${MY_DIR}/../../.."
 
-HELPER="${HAVOC_ROOT}/vendor/havoc/build/tools/extract_utils.sh"
+HELPER="${HAVOC_ROOT}/vendor/nusantara/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
